71c9ab05106d31ccf504357d044dee82d11691ee
Gunther Klessinger <gunther.klessinger@axiros.com>
2016-11-16 17:23:53 +0100

    manual

--- tests/manual/run_all.py
+++ tests/manual/run_all.py
@@ -5,7 +5,7 @@ sys.argv.append('')
 # substring match on filenames, if not given run all:
 match = sys.argv[1]
 
-exe = '../mdv/markdownviewer.py'
+exe = '../../mdv/markdownviewer.py'
 for file in os.listdir('.'):
     if not file.endswith('.md') or not match in file:
         continue
