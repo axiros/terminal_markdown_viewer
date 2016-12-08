fef63090ac413bc8c7ecee0f1e4893bc88ea1939
Gunther Klessinger <gk@axiros.com>
2016-12-08 14:13:34 +0100

    coverage / nose

---
+++ mdv/tests/test_files.py
@@ -0,0 +1,41 @@
+from unittest import TestCase, main
+try:
+    import sys; reload(sys); sys.setdefaultencoding('utf-8')
+except:
+    pass # py3
+import os
+import mdv
+
+here = os.path.abspath(__file__).rsplit('/', 1)[0]
+
+class TestNose(TestCase):
+    ''' testing the test'''
+    def test_itself(self):
+        self.assertTrue(True)
+
+
+class TestFiles(TestCase):
+    '''tests all .md files within the files directory against their build
+    version'''
+    def test_all(self):
+        df = here + '/files'
+        for f in os.listdir(df):
+            if not f.endswith('.md'):
+                continue
+            print (f)
+            with open(df + '/' + f) as fd:
+                src = fd.read()
+            for col in 20, 40, 80, 200:
+                print (col)
+                rd = 'result.%s' % col
+                res = mdv.main(src, cols=col, theme=729.8953, c_theme=729.8953)
+                with open('%s/result.%s/%s.expected' % (df, col, f)) as fd:
+                    tgt = fd.read()
+                print (res)
+                if not unicode(tgt).strip() == unicode(res).strip():
+                    raise Exception("Error %s col %s" % (f, col))
+
+
+
+if __name__ == '__main__':
+    main()
