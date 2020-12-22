83cde8d99502b60208c6aac63b7e21c5d1f4a00f
liudonghua <liudonghua123@gmail.com>
2020-12-22 14:17:23 +0800

    set the encoding of open explicitly

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup, find_packages
 
 version = "1.7.4"
 
-with open(os.path.join(os.path.dirname(__file__), "README.md")) as fd:
+with open(os.path.join(os.path.dirname(__file__), "README.md"), encoding='utf-8') as fd:
     md = fd.read()
 
 # images hack for pypi:
